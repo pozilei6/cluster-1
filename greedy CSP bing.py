2c8693637183960a4a31c61799b6650f487ef50c
pozilei6 <daniel.schwegler@gmx.ch>
2023-08-22 15:21:19 +0200

    Update greedy CSP bing.py

--- greedy CSP bing.py
+++ greedy CSP bing.py
@@ -148,6 +148,218 @@ a method in a python function?
 
 
 
+# run in  https://www.onlinegdb.com/online_python_compiler
+
+import numpy as np 
+from multiprocessing import Process, Manager
+
+
+large_width = 400
+np.set_printoptions(linewidth=large_width)
+
+
+
+D = [7, 11, 19, 35, 67, 131, 13, 21, 37, 69, 133, 25, 41, 73, 137, 49, 81, 145, 97, 161, 193, 14, 22, 38,
+              70, 134, 26, 42, 74, 138, 50, 82, 146, 98, 162, 194, 28, 44, 76, 140, 52, 84, 148, 100, 164, 196, 56,
+              88, 152, 104, 168, 200, 112, 176, 208, 224]
+A = np.array(
+    [#1 2 3 4 5 6 7 8 9 10111213141516171819
+     [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1], # 1   d
+     [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # 2  d
+     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0], # 3   d
+     [0,0,1,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0], # 4  d
+     [1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0], # 5 d
+     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0], # 6   d
+     [0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0], # 7     d
+     [1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0], # 8 d
+     [0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,1,0,0], # 9   d
+     [0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0], # 10  d
+     [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0], # 11    d
+     [0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0], # 12  d
+     [1,0,0,0,0,0,0,0,1,1,1,0,0,0,1,0,0,0,0], # 13    d
+     [0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0], # 14    d
+     [1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,1], # 15 d
+     [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0], # 16   d
+     [0,0,0,0,1,0,1,1,0,1,0,1,0,0,1,0,1,0,0], # 17 d
+     [0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0], # 18 d
+     [1,0,1,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,0], # 19 d
+     [0,0,0,0,0,1,1,0,1,0,0,0,0,1,1,0,0,0,0], # 20 d
+     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], # 21  d
+     [0,0,0,0,0,1,0,0,1,0,1,0,0,0,1,0,0,0,0], # 22 d
+     [0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,1], # 23  d
+     [1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,0], # 24 d
+     ],dtype = int)
+
+
+
+def find_approximate_solution(A, D, best_R, best_C, best_count):
+    def backtrack(A, D, sort_C_ind, start_C_index, C_prev, R, C):
+        m, n = A.shape
+        if start_C_index == n:
+            count = sum([R[i] & C[j] == C[j] for i in range(m) for j in range(n) if A[i, j] == 1])
+            if count > best_count[0]:
+                best_R[:] = R
+                best_C[:] = C
+                best_count[0] = count
+            return
+        j = sort_C_ind[start_C_index]
+        if A[:, j].sum() == n:
+            C[j] = 0
+            backtrack(A, D, sort_C_ind, start_C_index + 1, C_prev, R, C)
+        else:
+            for k in range(n):
+                C[j] = 1 << k
+                R_new = list(R)
+                for i in range(m):
+                    if A[i, j] == 1:
+                        R_new[i] |= C[j]
+                        if not any([R_new[i] & d == R_new[i] for d in D]):
+                            break
+                        for jj in range(n):
+                            if A[i, jj] == 1:
+                                C[jj] &= R_new[i]
+                else:
+                    backtrack(A, D, sort_C_ind, start_C_index + 1, C_prev, R_new, C)
+
+    m, n = A.shape
+    sort_C_ind = np.argsort(-A.sum(axis=0))
+    R = [0] * m
+    C = [0] * n
+    backtrack(A, D, sort_C_ind, 0, C[:], R[:], C[:])
+
+def find_approximate_solution_with_timeout(A, D, timeout):
+    with Manager() as manager:
+        best_R = manager.list([0] * A.shape[0])
+        best_C = manager.list([0] * A.shape[1])
+        best_count = manager.list([0])
+        p = Process(target=find_approximate_solution, args=(A, D, best_R, best_C, best_count))
+        p.start()
+        p.join(timeout)
+        p.terminate()
+        return list(best_R), list(best_C)
+        
+        
+        
+        
+        
+timeout = 100
+R, C = find_approximate_solution_with_timeout(A, D, timeout)     
+
+print(R, C)
+
+F = np.empty(shape=(m,n),dtype=bool)
+for i in range(m):
+    for j in range(n):
+        F[i, j] = (R[i] & C[j] == C[j]) == A[i, j]
+
+m,n=A.shape
+print(F, np.sum(F)/(n*m))
+        
+        
+        
+        
+######################################################################################################################## 
+# run in  https://www.onlinegdb.com/online_python_compiler
+
+import random
+import numpy as np 
+
+large_width = 400
+np.set_printoptions(linewidth=large_width)
+
+D = [7, 11, 19, 35, 67, 131, 13, 21, 37, 69, 133, 25, 41, 73, 137, 49, 81, 145, 97, 161, 193, 14, 22, 38,
+              70, 134, 26, 42, 74, 138, 50, 82, 146, 98, 162, 194, 28, 44, 76, 140, 52, 84, 148, 100, 164, 196, 56,
+              88, 152, 104, 168, 200, 112, 176, 208, 224]
+A = np.array(
+    [#1 2 3 4 5 6 7 8 9 10111213141516171819
+     [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1], # 1   d
+     [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], # 2  d
+     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0], # 3   d
+     [0,0,1,0,1,0,1,0,0,0,0,0,0,1,0,0,0,0,0], # 4  d
+     [1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0], # 5 d
+     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,0], # 6   d
+     [0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0], # 7     d
+     [1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0], # 8 d
+     [0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,1,0,0], # 9   d
+     [0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0], # 10  d
+     [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0], # 11    d
+     [0,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0], # 12  d
+     [1,0,0,0,0,0,0,0,1,1,1,0,0,0,1,0,0,0,0], # 13    d
+     [0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0], # 14    d
+     [1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,1,1], # 15 d
+     [0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0], # 16   d
+     [0,0,0,0,1,0,1,1,0,1,0,1,0,0,1,0,1,0,0], # 17 d
+     [0,0,0,0,0,0,0,1,0,0,0,1,0,1,0,0,0,0,0], # 18 d
+     [1,0,1,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,0], # 19 d
+     [0,0,0,0,0,1,1,0,1,0,0,0,0,1,1,0,0,0,0], # 20 d
+     [0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], # 21  d
+     [0,0,0,0,0,1,0,0,1,0,1,0,0,0,1,0,0,0,0], # 22 d
+     [0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,1], # 23  d
+     [1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,0], # 24 d
+     ],dtype = int)
+
+
+
+def fitness(R, C, A):
+    m, n = A.shape
+    return sum([R[i] & C[j] == C[j] for i in range(m) for j in range(n) if A[i, j] == 1])
+
+def crossover(R1, C1, R2, C2):
+    m = len(R1)
+    n = len(C1)
+    R = [R1[i] if random.random() < 0.5 else R2[i] for i in range(m)]
+    C = [C1[j] if random.random() < 0.5 else C2[j] for j in range(n)]
+    return R, C
+
+def mutate(R, C, D):
+    m = len(R)
+    n = len(C)
+    i = random.randrange(m)
+    R[i] = random.choice(D)
+    j = random.randrange(n)
+    C[j] = 1 << random.randrange(8)
+
+def find_approximate_solution(A, D):
+    m, n = A.shape
+    population_size = 100
+    num_generations = 1000
+    population_R = [[random.choice(D) for _ in range(m)] for _ in range(population_size)]
+    population_C = [[1 << random.randrange(8) for _ in range(n)] for _ in range(population_size)]
+    for generation in range(num_generations):
+        fitness_values = [fitness(population_R[i], population_C[i], A) for i in range(population_size)]
+        population_R = [x for _, x in sorted(zip(fitness_values, population_R), reverse=True)]
+        population_C = [x for _, x in sorted(zip(fitness_values, population_C), reverse=True)]
+        new_population_R = population_R[:population_size // 2]
+        new_population_C = population_C[:population_size // 2]
+        while len(new_population_R) < population_size:
+            i = random.randrange(population_size // 2)
+            j = random.randrange(population_size // 2)
+            R, C = crossover(population_R[i], population_C[i], population_R[j], population_C[j])
+            mutate(R, C, D)
+            new_population_R.append(R)
+            new_population_C.append(C)
+        population_R = new_population_R
+        population_C = new_population_C
+    best_R = population_R[0]
+    best_C = population_C[0]
+    return best_R[:], best_C[:]
+    
+    
+R, C = find_approximate_solution(A, D)
+    
+print(R, C)
+
+m,n=A.shape
+F = np.empty(shape=(m,n),dtype=bool)
+for i in range(m):
+    for j in range(n):
+        F[i, j] = (R[i] & C[j] == C[j]) == A[i, j]
+
+print(F, np.sum(F)/(n*m))  #np.sum(F)/(n*m) = 0.546
+##########################################################################################################################
+        
+        
+        
 
 
 
