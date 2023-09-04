ce5274e37a8e54a7c7ec3f1d2373d7a412aadd3a
pozilei6 <daniel.schwegler@gmx.ch>
2023-09-04 17:00:44 +0200

    Update DAG assignment problem.py

--- DAG assignment problem.py
+++ DAG assignment problem.py
@@ -76,3 +76,76 @@ def dag_set_labeling(M, M_trr, K=None):
         # No solution was found
         return None
 #-----------------------------------------------
+
+
+
+
+
+
+
+
+
+#----------------------------------------------- finishing
+def CSP_ass(assignment, A, D, bitlength):
+    m, n = A.shape
+    defa = 2 ** bitlength - 1
+    all_C_inds = set.union(*assignment)
+
+    R = [BitVec(f"r_{i}", bitlength) for i in range(m)]
+    C = [BitVec(f"c_{j}", bitlength) for j in range(n)]
+
+    s = Solver()
+
+    for i in range(m):
+        s.add(Or([R[i] == d for d in D]))
+
+    for j in range(n):
+        s.add(C[j] == reduce(lambda x,y: x & y, [R[i] for i in range(m) if A[i, j] == 1], defa))
+
+    for i in range(m):
+        for j in range(n):
+            if A[i, j] == 1:
+                s.add(R[i] & C[j] == C[j])
+            if A[i, j] == 0:
+                s.add(R[i] & C[j] != C[j])
+
+    # Add the missing constraint
+    for k in all_C_inds:
+        # Find the unique bit position for value k
+        b_k = BitVec(f"b_{k}", bitlength)
+        s.add(And([b_k == (C[j] & (C[j] - 1)) ^ C[j] for j in range(n) if k in assignment[j]]))
+        
+        # Ensure that the unique bit position is different for any other value k1
+        for k1 in all_C_inds:
+            if k != k1:
+                b_k1 = BitVec(f"b_{k1}", bitlength)
+                s.add(b_k != b_k1)
+
+
+
+"""
+In this code, the missing constraint is added to the solver using a nested loop over all values k and k1 in all_C_inds. For each value k, a new bit vector 
+variable b_k is created to represent the unique bit position where all C[j] with k in assignment[j] have a set bit. This variable is constrained to be equal 
+to the result of the expression (C[j] & (C[j] - 1)) ^ C[j], which extracts the rightmost set bit of C[j]. The constraint is added for all j such that k is 
+in assignment[j]. Then, for any other value k1 in all_C_inds, a new bit vector variable b_k1 is created to represent the unique bit position for value k1, 
+and a constraint is added to ensure that b_k != b_k1.
+"""
+#-----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
