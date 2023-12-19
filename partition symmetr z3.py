337be72260e2a2ced9bf5fce5321682acb67a919
pozilei6 <daniel.schwegler@gmx.ch>
2023-12-19 17:18:36 +0100

    Update partition symmetr z3.py

--- partition symmetr z3.py
+++ partition symmetr z3.py
@@ -1,1 +1,35 @@
+from z3 import *
 
+# Define m
+m = 9  # You can set this to whatever value you want
+
+# Create the lists of integer variables
+belongs_to_cluster = [Int('b_%i' % i) for i in range(m)]
+index_within_its_cluster = [Int('w_%i' % i) for i in range(m)]
+
+# Create a solver instance
+s = Solver()
+
+# Add constraints for belongs_to_cluster
+for b in belongs_to_cluster:
+    s.add(b >= -1, b < m//3)
+
+# Add constraints for index_within_its_cluster
+for w in index_within_its_cluster:
+    s.add(w >= -1, w < m//2)
+
+# Add distinctness constraint for belongs_to_cluster excluding -1
+for i in range(len(belongs_to_cluster)):
+    for j in range(i + 1, len(belongs_to_cluster)):
+        s.add(Implies(And(belongs_to_cluster[i] >= 0, belongs_to_cluster[j] >= 0), belongs_to_cluster[i] != belongs_to_cluster[j]))
+
+# Add constraint that for different i, j, belongs_to_cluster[i] == belongs_to_cluster[j] implies that index_within_its_cluster[i] != index_within_its_cluster[j]
+for i in range(len(belongs_to_cluster)):
+    for j in range(i + 1, len(belongs_to_cluster)):
+        s.add(Implies(belongs_to_cluster[i] == belongs_to_cluster[j], index_within_its_cluster[i] != index_within_its_cluster[j]))
+
+# Check if the problem is satisfiable and print a possible model
+if s.check() == sat:
+    print(s.model())
+else:
+    print("The problem is not satisfiable")
